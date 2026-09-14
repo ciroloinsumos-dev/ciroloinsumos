0ab637b46e9988c46b65ecb0f9ca211d87ac1059
ciroloinsumos-dev <ciroloinsumos@gmail.com>
2026-09-14 19:08:17 -0300

    v2

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 
 const CONFIG = {
 
-    API_URL: "https://script.google.com/macros/s/AKfycbwPzy30zRRL8EDp_2FI6A4KPXN-jErd7KfLzyfryY0b-PZPvoM_9-flAakgrXveWa0hiw/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbwU2Z3rQZ4WbxGoqAFV-w6Z7mz0_2lUWAJV47qdPzgdcaxAdVUJOG6bv3mrRUWlPlAe2A/exec",
 
     MONEDA: "$",
 
